145b50d441e58da9a1010a6d191483b087047e70
zhao <754251092@qq.com>
2020-10-16 10:49:14 +0800

    volatile测试

--- app/src/test/java/monitordemo/demo/myapp/myapplication/ExampleUnitTest.java
+++ app/src/test/java/monitordemo/demo/myapp/myapplication/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package monitordemo.demo.myapp.myapplication;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,56 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(5, 2 + 3);
+    }
+
+    private volatile boolean flag;
+
+    @Test
+    public void fun2() {
+        //volatile的高速缓存 测试
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (flag) {
+                        System.out.println("set flag to false");
+                        flag = false;
+                    }
+                }
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (!flag) {
+                        System.out.println("set flag to true");
+                        flag = true;
+                    }
+                }
+            }
+        }.start();
+    }
+
+    private String value;
+
+    @Test
+    public void fun3() {
+        //volatile的禁止指令重排 测试
+        new Thread() {
+            @Override
+            public void run() {
+                value = "hello";
+                flag = true;
+            }
+        }.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (flag) {
+                    System.out.println(value.toUpperCase());
+                }
+            }
+        }).start();
     }
 }
