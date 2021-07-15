dc81161324389f366d2703dbc4d5e6de65643279
zhao <754251092@qq.com>
2021-07-15 15:29:23 +0800

    203. 移除链表元素：增加一种最本能的解法

--- app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/RemoveElements203.java
+++ app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/RemoveElements203.java
@@ -7,7 +7,35 @@ import java.util.List;
  * @Author zhao on 5/5/21
  */
 public class RemoveElements203 {
+    /**
+     * 列表中的节点数目在范围 [0, 104] 内
+     * 1 <= Node.val <= 50
+     * 0 <= val <= 50
+     */
+    public ListNode removeElements3(ListNode head, int val) {
+        //是在原链表的基础上进行操作，最本能的操作
+        ListNode newHead = null;
+        ListNode last = null;
+        while (head != null) {
+            if (head.val == val) {
+                //当满足条件时，需要修改上下节点之间的关系
+                if (last != null) {
+                    last.next = head.next;
+                }
+            } else {
+                //正常节点时，移动last即可；同时记录newHead
+                last = head;
+                if (newHead == null) {
+                    newHead = head;
+                }
+            }
+            head = head.next;
+        }
+        return newHead;
+    }
+
     public ListNode removeElements2(ListNode head, int val) {
+        //其实是构造一条新链
         ListNode zero = new ListNode(-1);
         ListNode last = zero;
         while (head != null) {
