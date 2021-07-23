75583e1306178d509c64c0a34212646245257b33
zhao <754251092@qq.com>
2021-07-23 14:57:56 +0800

    206. 反转链表-- 补充两种解法

--- app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/ListNode.java
+++ app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/ListNode.java
@@ -10,4 +10,37 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+
+    /**
+     * 迭代方式翻转
+     */
+    public static ListNode reverse(ListNode head) {
+        //依次遍历就可以，比较绕，但是可以充分理解面向对象
+        ListNode prev = null;
+        while (head != null) {
+            //先记录next
+            ListNode next = head.next;
+            //更改head.next，顺便切断了head和next之间的联系
+            head.next = prev;
+            //移动pre指针指向head
+            prev = head;
+            //移动head指针到next
+            head = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 递归方式翻转
+     */
+    public static ListNode reverseList2(ListNode head) {
+        //递归
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
 }
--- app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/ReverseList206.java
+++ app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/ReverseList206.java
@@ -7,6 +7,59 @@ import java.util.List;
  * @Author zhao on 5/10/21
  */
 public class ReverseList206 {
+    /**
+     * 链表中节点的数目范围是 [0, 5000]
+     * -5000 <= Node.val <= 5000
+     *  
+     * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+     */
+    public ListNode reverseList3(ListNode head) {
+        //递归
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = reverseList3(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+
+    public ListNode reverseList4(ListNode head) {
+        //迭代: 实际实在迭代中构造了一个新的链表
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = new ListNode(head.val);
+        while (head.next != null) {
+            //先构造出next对应的节点
+            ListNode te = new ListNode(head.next.val);
+            //将te和pre建立联系
+            te.next = pre;
+            //将pre指向te
+            pre = te;
+
+            //迭代链表
+            head = head.next;
+        }
+        return pre;
+    }
+
+    public ListNode reverseList5(ListNode head) {
+        //迭代:
+        ListNode pre = null;
+        while (head != null) {
+            //先记录next
+            ListNode next = head.next;
+            //更改head.next，顺便切断了head和next之间的联系
+            head.next = pre;
+            //移动pre指针指向head
+            pre = head;
+            //移动head指针到next
+            head = next;
+        }
+        return pre;
+    }
+
     public ListNode reverseList2(ListNode head) {
         //依次遍历就可以，比较绕，但是可以充分理解面向对象
         ListNode prev = null;
