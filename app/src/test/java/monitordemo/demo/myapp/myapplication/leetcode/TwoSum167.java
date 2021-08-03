40981941ace788403f86998fa55ba752d008f282
zhao <754251092@qq.com>
2021-08-03 14:58:11 +0800

    lc1. 两数之和--增加一种解法

--- app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/TwoSum167.java
+++ app/src/test/java/monitordemo/demo/myapp/myapplication/leetcode/TwoSum167.java
@@ -1,11 +1,44 @@
 package monitordemo.demo.myapp.myapplication.leetcode;
 
+import org.junit.Test;
+
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author zhao on 5/1/21
  */
 public class TwoSum167 {
+
+    @Test
+    public void fun() {
+        int[] nums = {3, 2, 4};
+        int t = 6;
+        System.out.println(Arrays.toString(twoSum10(nums, t)));
+    }
+
+    /**
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+     */
+    public int[] twoSum10(int[] nums, int target) {
+        //利用map来缩减寻找的时间
+        Map<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            int key = target - nums[i];
+            if (map.containsKey(key)) {
+                return new int[]{i, map.get(key)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
 
     /**
      * 2 <= numbers.length <= 3 * 104
